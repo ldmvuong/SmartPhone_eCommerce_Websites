c39c1d82bfb71671b93abf71b532ef62ca5cc82d
ldmvuong <minhvuongakira@gmail.com>
2024-09-16 11:34:19 +0700

    init project

---
+++ src/main/java/vn/ute/smartphoneshop/SmartPhoneShopApplication.java
@@ -0,0 +1,13 @@
+package vn.ute.smartphoneshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPhoneShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPhoneShopApplication.class, args);
+	}
+
+}
