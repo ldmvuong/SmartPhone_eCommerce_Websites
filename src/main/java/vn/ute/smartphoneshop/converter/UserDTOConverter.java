1a6691b76d7b6a0e10c3ab21454a1e83d68de2f6
Thị Hoàng Duyên Đỗ <thihoangduyendo@gmail.com>
2024-10-22 17:05:34 +0700

    show list-user

--- src/main/java/vn/ute/smartphoneshop/converter/UserDTOConverter.java
+++ src/main/java/vn/ute/smartphoneshop/converter/UserDTOConverter.java
@@ -1,5 +1,6 @@
 package vn.ute.smartphoneshop.converter;
 
+import jakarta.annotation.PostConstruct;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,14 @@ public class UserDTOConverter {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @PostConstruct
+    public void init() {
+        // Ánh xạ từ UserEntity sang UserDTO, lấy roleName từ RoleEntity
+        modelMapper.typeMap(UserEntity.class, UserDTO.class).addMappings(mapper -> {
+            mapper.map(src -> src.getRole().getRoleId(), UserDTO::setRoleId);
+        });
+    }
 
     public UserDTO toUserDTO(UserEntity item) {
         UserDTO userDTO = modelMapper.map(item, UserDTO.class);
--- src/main/java/vn/ute/smartphoneshop/repository/custom/UserRepositoryCustom.java
+++
@@ -1,6 +0,0 @@
-package vn.ute.smartphoneshop.repository.custom;
-
-import vn.ute.smartphoneshop.repository.UserRepository;
-
-public class UserRepositoryCustom {
-}
