f298914e541acae43ecd30d097e5567e75864b79
ldmvuong <minhvuongakira@gmail.com>
2024-12-01 21:20:57 +0700

    Order history in my account

---
+++ src/main/java/vn/ute/smartphoneshop/model/dto/MyOrderDetailDTO.java
@@ -0,0 +1,14 @@
+package vn.ute.smartphoneshop.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class MyOrderDetailDTO {
+    private String productName;
+    private int quantity;
+    private BigDecimal unitPrice;
+}
--- src/main/java/vn/ute/smartphoneshop/repository/OrderRepository.java
+++ src/main/java/vn/ute/smartphoneshop/repository/OrderRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vn.ute.smartphoneshop.entity.OrderEntity;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity,Integer> {
+    List<OrderEntity> findByUser_UserIdOrderByOrderDateDesc(int customerId);
 }
