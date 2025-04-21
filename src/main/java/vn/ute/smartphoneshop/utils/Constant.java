60e93b857fa217b6bf5a68cfb72f6781aca45348
ldmvuong <minhvuongakira@gmail.com>
2025-04-21 09:16:18 +0700

    "Fix Locale.of() to be compatible with Java 17"

--- src/main/java/vn/ute/smartphoneshop/utils/Constant.java
+++ src/main/java/vn/ute/smartphoneshop/utils/Constant.java
@@ -4,7 +4,7 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 public class Constant {
-    public static Locale vietnam = Locale.of("vi", "VN");
+    public static Locale vietnam = new Locale("vi", "VN");
     // Định dạng số thành tiền tệ Việt Nam Đồng
     public static NumberFormat formatter = NumberFormat.getCurrencyInstance(vietnam);
 }
